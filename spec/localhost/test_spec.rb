fe39a2d43a555974b4f985096e896f5cfa610f88
Alex Harvey <alexharv074@gmail.com>
2019-08-17 01:53:22 +1000

    Further cleanup

--- spec/localhost/test_spec.rb
+++ spec/localhost/test_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe command('/usr/local/bin/terraform0126 output') do
+describe command('terraform output') do
   its(:stdout) { should match /http/ }
 end
 
-hello_url = %x{/usr/local/bin/terraform0126 output}.split[2].chomp
+hello_url = %x{terraform output}.split[2].chomp
 
 describe command("curl #{hello_url}") do
   its(:stdout) { should match /hello world/ }
--- test_runner.sh
+++ test_runner.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
-terraform init
-terraform apply -auto-approve
-bundle install
-bundle exec rspec -f d spec/localhost/test_spec.rb
-terraform destroy -auto-approve
+
+if ! command -v terraform ; then
+  echo "Terraform binary not found."
+  exit 1
+fi
+
+if ! command -v bundle ; then
+  echo "Bundler does not appear to be installed."
+  exit 1
+fi
+
+deploy() {
+  terraform init
+  terraform apply -auto-approve
+}
+
+wait_for_service() {
+  sleep 30
+}
+
+run_tests() {
+  bundle install
+  bundle exec rspec -f d spec/localhost/test_spec.rb
+}
+
+teardown() {
+  terraform destroy -auto-approve
+}
+
+deploy
+wait_for_service
+run_tests
+teardown
+
+# vim: set ft=sh:
